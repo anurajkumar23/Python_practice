1c977bf248c2c21a5a96c06cc54ba69277801f66
Anuraj Kumar <anurajkumar6294@gmail.com>
2023-01-19 14:50:54 +0530

    Rock_Paper_Scissors_game

---
+++ Day4/Tresuremap_row_column.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+#23
+#Write your code below this row 👇
+horizontal= int(position[0])#2
+vertical =int(position[1])#3
+
+map[vertical -1 ][horizontal - 1] = "X"
+
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
+
