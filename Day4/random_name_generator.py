bd89583ff526b1ff4726a52fedbed3ae904b39ef
Anuraj Kumar <anurajkumar6294@gmail.com>
2023-01-19 13:51:46 +0530

    random_name_generator

---
+++ Day4/random_name_generator.py
@@ -0,0 +1,17 @@
+import random
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+
+#Write your code below this line 👇
+
+#Get the total number of items in list.
+num_items = len(names)
+#Generate random numbers between 0 and the last index. 
+random_choice = random.randint(0, num_items - 1)
+#Pick out random person from list of names using the random number.
+person_who_will_pay = names[random_choice]
+
+print(person_who_will_pay + " is going to buy the meal today!")
+
