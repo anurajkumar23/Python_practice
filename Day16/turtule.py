22026cb8eb5420f78c7243dc1d5d554d17856ba2
Anuraj Kumar <anurajkumar6294@gmail.com>
2023-02-07 23:38:47 +0530

    Day16

---
+++ Day16/turtule.py
@@ -0,0 +1,21 @@
+# from turtle import Screen, Turtle
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("blue")
+# timmy.forward(100)
+
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+# from prettytable import PrettyTable
+# table = PrettyTable()
+
+# table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
+# table.add_column("Type",["Electic","Water","Fire"])
+
+# table.align = "l"
+
+# print(table)
