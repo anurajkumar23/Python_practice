7b6b1163bde62fb6dd10fe5e1444d355481caf64
Anuraj Kumar <anurajkumar6294@gmail.com>
2023-03-22 23:48:53 +0530

    Day22

--- Day20/Sanke_game/snake_body.py
+++ Day20/Sanke_game/snake_body.py
@@ -38,10 +38,8 @@ while is_game_on:
         scoreboard.game_over()
 
     # detecting collision with tail.
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             is_game_on =False
             scoreboard.game_over()
 
