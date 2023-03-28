c90579044edece783f2005f646036addee4a0d46
Anuraj Kumar <anurajkumar6294@gmail.com>
2023-03-28 17:37:17 +0530

    Day24

--- Day24/Snake_game/food.py
+++ Day24/Snake_game/food.py
@@ -1,19 +1,19 @@
 from turtle import Turtle
 import random
 
+
 class Food(Turtle):
+
     def __init__(self):
         super().__init__()
         self.shape("circle")
         self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
-        self.shapesize(stretch_len=0.5,stretch_wid=0.5)
         self.refresh()
-    
-    def refresh(self):
-        random_X = random.randint(-280,280)
-        random_Y = random.randint(-280,280)
-        self.goto(random_X, random_Y)
-        
 
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
--- Day24/Snake_game/main.py
+++ Day24/Snake_game/main.py
@@ -1,18 +1,19 @@
 from turtle import Screen
-import time
 from snake import Snake
 from food import Food
-from score_board import Score_board
+from scoreboard import Scoreboard
+import time
 
-screen =Screen()
-screen.setup(width=600,height=600)
+
+screen = Screen()
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
 food = Food()
-scoreboard =Score_board()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,29 +21,30 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-is_game_on = True
-while is_game_on:  
+game_is_on = True
+while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
 
-    # detecting collision with food.
+    #Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
 
-    # detecting collision with wall.
+    #Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         scoreboard.reset()
         snake.reset()
 
-    # detecting collision with tail.
+    #Detect collision with tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             scoreboard.reset()
             snake.reset()
-            
+
+
 
 
 
