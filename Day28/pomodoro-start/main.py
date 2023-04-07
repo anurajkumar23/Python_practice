36b0cc864e59847529ae745abf24cb1ef4cfe112
Anuraj Kumar <anurajkumar6294@gmail.com>
2023-04-07 15:44:00 +0530

    Day28

--- Day28/pomodoro-start/main.py
+++ Day28/pomodoro-start/main.py
@@ -20,11 +20,23 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+title_label = Label(text="Timer", fg=GREEN,bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+start_button= Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button= Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2,row=2)
+
+check_marks = Label(text="✔", bg=YELLOW, fg= GREEN)
+check_marks.grid(column=1,row=3)
 
 
 
---
+++ Day28/pomodoro-start/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+start_button= Button(text="Start")
+# # start_button.grid(column=)
+# reset_button= Button(text="Reset")
