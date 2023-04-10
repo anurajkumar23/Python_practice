883421c6ce8cda0836a1cfc70a639c4dcb868131
Anuraj Kumar <anurajkumar6294@gmail.com>
2023-04-10 20:23:04 +0530

    Day29

---
+++ Day29/Password-generator/main.py
@@ -0,0 +1,22 @@
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.pack()
+
+
+
+
+
+
+window.mainloop()
