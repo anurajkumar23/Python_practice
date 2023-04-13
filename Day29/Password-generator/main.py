84780f88c72fecc1f8e257fffbcf266de5243b36
Anuraj Kumar <anurajkumar6294@gmail.com>
2023-04-13 10:35:51 +0530

    Day29

--- Day29/Password-generator/main.py
+++ Day29/Password-generator/main.py
@@ -2,6 +2,15 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+
+    website = website_entries.get()
+    email = email_entries.get()
+    password = password_entries.get()
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        website_entries.delete(0, END)
+        password_entries.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -25,8 +34,10 @@ password_label.grid(row=3, column=0)
 #Entries
 website_entries = Entry(width=36)
 website_entries.grid(row=1, column=1, columnspan=2)
+website_entries.focus()
 email_entries = Entry(width=36)
 email_entries.grid(row=2, column=1, columnspan=2)
+email_entries.insert(0, "anurajkumar6294@gmail.com")
 password_entries= Entry(width=18)
 password_entries.grid(row=3, column=1)
 
@@ -34,7 +45,7 @@ password_entries.grid(row=3, column=1)
 
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add",width=36)
+add_button = Button(text="Add",width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 
