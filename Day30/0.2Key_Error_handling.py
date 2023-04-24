0ef6474958cc068eb9f963f324b5779cb50a33b9
Anuraj Kumar <anurajkumar6294@gmail.com>
2023-04-24 20:22:44 +0530

    Day30

--- Day30/0.2Key_Error_handling.py
+++ Day30/0.2Key_Error_handling.py
@@ -3,7 +3,7 @@ facebook_posts = [
     {'Likes': 13, 'Comments': 2, 'Shares': 1}, 
     {'Likes': 33, 'Comments': 8, 'Shares': 3}, 
     {'Comments': 4, 'Shares': 2}, 
-    {'Comments': 1, 'Shares': 1}, 
+    {'Comments': 1, 'Shares': 1},  
     {'Likes': 19, 'Comments': 3}
 ]
 
