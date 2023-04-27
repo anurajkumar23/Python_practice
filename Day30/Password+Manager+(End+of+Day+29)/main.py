ecc535bd39573ecac508a349bcd100fa0d4fe2b1
Anuraj Kumar <anurajkumar6294@gmail.com>
2023-04-27 22:48:04 +0530

    Day30

--- Day30/Password+Manager+(End+of+Day+29)/main.py
+++ Day30/Password+Manager+(End+of+Day+29)/main.py
@@ -39,15 +39,20 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        with open("data.json", "r") as data_file:
-                    #reading the old data
-                    data = json.load(data_file)
+        try:
+            with open("data.json", "r") as data_file:
+                        #reading the old data
+                        data = json.load(data_file)
+        except FileExistsError:
+                    with open("data.json","w") as data_file:
+                           json.dump(new_data,data_file, indent=4)
+        else:                  
                     #updating old data with new data
                     data.update(new_data)
-        with open("data.json","w") as data_file:        
-                    #saving updated data
-                    json.dump(data, data_file, indent=4)
-                    
+                    with open("data.json","w") as data_file:        
+                                #saving updated data
+                                json.dump(data, data_file, indent=4)
+        finally:          
 
                     website_entry.delete(0, END)
                     password_entry.delete(0, END)
