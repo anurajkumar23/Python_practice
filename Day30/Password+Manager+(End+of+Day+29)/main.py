2c890cc07b858efdc3c6a00c08dedada27c3e57e
Anuraj Kumar <anurajkumar6294@gmail.com>
2023-04-27 22:20:52 +0530

    Day30

--- Day30/Password+Manager+(End+of+Day+29)/main.py
+++ Day30/Password+Manager+(End+of+Day+29)/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -28,17 +29,29 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+          website:{
+                "email": email,
+                "password:": password,
+          }
+          }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
-                                                      f"\nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        with open("data.json", "r") as data_file:
+                    #reading the old data
+                    data = json.load(data_file)
+                    #updating old data with new data
+                    data.update(new_data)
+        with open("data.json","w") as data_file:        
+                    #saving updated data
+                    json.dump(data, data_file, indent=4)
+                    
+
+                    website_entry.delete(0, END)
+                    password_entry.delete(0, END)
+                   
 
 
 # ---------------------------- UI SETUP ------------------------------- #
