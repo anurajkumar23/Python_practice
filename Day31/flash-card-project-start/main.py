a1aed6b272f9fdf6695f6937d5daed9c19ae9467
Anuraj Kumar <anurajkumar6294@gmail.com>
2023-04-28 17:56:51 +0530

    Day31

---
+++ Day31/flash-card-project-start/main.py
@@ -0,0 +1,25 @@
+from tkinter import *
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400,263, image=card_front_img)
+canvas.grid(row=0, column=0, columnspan=2)
+canvas.create_text(400,150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400,263, text="Word", font=("Ariel", 40, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+
+
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button =Button(image=cross_image, highlightthickness=0)
+unknown_button.grid(row=1,column=0)
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0)
+known_button.grid(row=1,column=1)
+
+window.mainloop()
