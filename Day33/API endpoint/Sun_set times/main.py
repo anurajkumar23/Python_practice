c18374e09e2c976f8a912b2c27e09207eb11416f
Anuraj Kumar <anurajkumar6294@gmail.com>
2023-05-22 20:50:15 +0530

    Day33

--- Day33/API endpoint/Sun_set times/main.py
+++ Day33/API endpoint/Sun_set times/main.py
@@ -2,23 +2,25 @@ import requests
 from datetime import datetime
 #My latitude and my longitude of my location
 
-My_LAT = 25.224300
-MY_LONG = 84.990211
+My_LAT = 23.249100
+MY_LONG = 87.869400
 
 
 parameters = {
     "lat": My_LAT,
-    "lng": MY_LONG
+    "lng": MY_LONG,
+    "formatted": 0,
 }
 
 response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
-sunrise = data["results"]["sunrise"]
-sunset = data["results"]["sunset"]
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
+print(sunrise)
 print(sunset)
 
 time_now = datetime.now()
 
-print(time_now)
+print(time_now.hour)
